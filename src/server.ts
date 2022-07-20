fdcff6d6f8e00682246d512de05ab6f675691a20
Saroj Shrestha <saroj.shr911@gmail.com>
2022-07-20 19:08:10 +0545

    playing with graphql with opine

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,16 @@
-import { Server } from "https://deno.land/std@0.148.0/http/server.ts";
+// deno-lint-ignore-file no-explicit-any
+import { opine, OpineRequest } from "https://deno.land/x/opine@2.2.0/mod.ts";
+
 import { GraphQLHTTP } from "https://deno.land/x/gql@1.1.2/mod.ts";
 import { makeExecutableSchema } from "https://deno.land/x/graphql_tools@0.0.2/mod.ts";
+
+import { readAll } from "https://deno.land/std@0.148.0/streams/conversion.ts";
 import { gql } from "https://deno.land/x/graphql_tag@0.0.1/mod.ts";
+
 import { config } from "https://deno.land/x/dotenv@v3.2.0/mod.ts";
 // const { PORT = 4000 } = config({ safe: true });
+
+type Request = OpineRequest & { json: () => Promise<any> };
 
 const typeDefs = gql`
   type Query {
@@ -11,22 +18,44 @@ const typeDefs = gql`
   }
 `;
 
-const resolvers = { Query: { hello: () => `Hello World!` } };
-
-const app = new Server({
-  handler: async (req) => {
-    const { pathname } = new URL(req.url);
-
-    return pathname === "/graphql"
-      ? await GraphQLHTTP<Request>({
-          schema: makeExecutableSchema({ resolvers, typeDefs }),
-          graphiql: true,
-        })(req)
-      : new Response("Not Found", { status: 404 });
+const resolvers = {
+  Query: {
+    hello: (
+      _root: undefined,
+      _args: unknown,
+      ctx: { request: Request },
+      info: { fieldName: string }
+    ) => {
+      return `Hello World from ${ctx.request.originalUrl}!. You have called ${info.fieldName}`;
+    },
   },
-  // port: Number(PORT),
-  hostname: "0.0.0.0",
-});
+};
 
-console.log("Server Running!");
-app.listenAndServe();
+const dec = new TextDecoder();
+
+const schema = makeExecutableSchema({ resolvers, typeDefs });
+
+const app = opine();
+
+app
+  .use("/graphql", async (req, res) => {
+    const request = req as Request;
+
+    request.json = async () => {
+      const rawBody = await readAll(req.raw);
+      return JSON.parse(dec.decode(rawBody));
+    };
+
+    const resp = await GraphQLHTTP<Request>({
+      schema,
+      context: (request) => ({ request }),
+      graphiql: true,
+    })(request);
+
+    for (const [k, v] of resp.headers.entries()) res.headers?.append(k, v);
+
+    res.status = resp.status;
+
+    res.send(await resp.text());
+  })
+  .listen(3000, () => console.log(`☁  Started on http://localhost:3000`));
