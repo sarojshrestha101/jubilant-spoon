198d1652fb16502da051cc9af816085b1550bca2
Saroj Shrestha <saroj.shr911@gmail.com>
2022-07-20 19:12:46 +0545

    issue with dynamic import

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 // deno-lint-ignore-file no-explicit-any
 import { opine, OpineRequest } from "https://deno.land/x/opine@2.2.0/mod.ts";
 
-import { GraphQLHTTP } from "https://deno.land/x/gql@1.1.2/mod.ts";
+import { GraphQLHTTP } from "./GraphQLHTTP.ts";
 import { makeExecutableSchema } from "https://deno.land/x/graphql_tools@0.0.2/mod.ts";
 
 import { readAll } from "https://deno.land/std@0.148.0/streams/conversion.ts";
@@ -58,4 +58,4 @@ app
 
     res.send(await resp.text());
   })
-  .listen(3000, () => console.log(`☁  Started on http://localhost:3000`));
+  .listen();
