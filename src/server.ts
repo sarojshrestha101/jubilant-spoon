82ce8008b385043d199fbfa840ab26ab54c10f5c
Saroj Shrestha <saroj.shr911@gmail.com>
2022-07-20 17:14:00 +0545

    second commit

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,18 @@
-// @ts-ignore
-import { Application } from "https://deno.land/x/oak/mod.ts";
-// @ts-ignore
-import { config } from "https://deno.land/x/dotenv/mod.ts";
-// @ts-ignore
-import { Logger, LoggerOptions } from "https://deno.land/x/deno_util/logger.ts";
+import { Application } from "https://deno.land/x/oak@v10.6.0/mod.ts";
+import { config } from "https://deno.land/x/dotenv@v3.2.0/mod.ts";
+import {
+  Logger,
+  LoggerOptions,
+} from "https://deno.land/x/deno_util@v0.0.3/logger.ts";
 
 const initialOptions = { level: 0, format: "%s", newLine: true };
 const logger = new Logger(initialOptions as LoggerOptions);
 const { PORT = 4000 } = config({ safe: true });
 
 const app = new Application();
-app.use((cxt: { response: { body: string } }) => {
+app.use((cxt) => {
   cxt.response.body = "Hello GraphQL";
 });
 
-logger.line(`🚀 Server is running on http://localhost:${PORT}/graphql`);
+logger.info("Server is running");
 await app.listen({ port: Number(PORT) });
